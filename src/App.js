4b1ddf329bb517b1428a39e81e19c7c8c5a49ac7
David Islam <david.islam24@gmail.com>
2022-10-02 14:42:56 -0700

    Handle search input with useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import { useState } from 'react'
+
 import Search from "./components/Search";
 
 function App() {
+  const [state, setState] = useState({
+    searchQuery: '',
+    results: [],
+  })
+  const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=be4b3906';
+  
+  const handleInput = e => {
+    const searchQuery = e.target.value; 
+    setState(prevState => {
+      return { ...prevState, searchQuery}
+    })
+  };
+  
   return (
     <div className="container">
       <header>
         <h1>Movie App</h1>
       </header>
       <main>
-        <Search />
+        <Search handleInput={handleInput}/>
       </main>
     </div>
   );
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-function Search() {
+function Search(props) {
   return (
     <section className='search-box-wrapper'>
-        <input type="text" placeholder='Search for a movie...' className="search-box" />
+        <input 
+          type="text" 
+          placeholder='Search for a movie...' 
+          className="search-box" 
+          onChange={props.handleInput}  
+        />
     </section>
   )
 }
