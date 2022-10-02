367773c264aa3def126c4662e71ca6348f1ec1a3
David Islam <david.islam24@gmail.com>
2022-10-02 11:54:15 -0700

    Create and style Search component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Search from "./components/Search";
+
 function App() {
   return (
     <div className="container">
       <header>
         <h1>Movie App</h1>
       </header>
+      <main>
+        <Search />
+      </main>
     </div>
   );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Search() {
+  return (
+    <section className='search-box-wrapper'>
+        <input type="text" placeholder='Search for a movie...' className="search-box" />
+    </section>
+  )
+}
+
+export default Search
